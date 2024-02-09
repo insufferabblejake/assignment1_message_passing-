410da397162d0ee4bd088543e6a0c246e20646e3
Karthik Jagadeesan <insufferablejake@gmail.com>
2024-02-08 18:26:55 -0800

    Mutliple threads all sending results to main. Waiting on channel to close fixed with count.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{
     sync::{mpsc, Arc},
     thread,
+    time::Duration,
 };
 
 struct Task {
@@ -28,6 +29,8 @@ impl Worker {
 
     // Need to wrap the task in Arc so that we can accept it from the main thread
     fn process_task(&self, task: Arc<Task>) -> String {
+        // simulate some random workload
+        thread::sleep(Duration::from_millis(rand::random::<u64>() % 100));
         format!(
             "Worker {} processed task {} with payload {}",
             self.id, task.id, task.payload
@@ -49,24 +52,42 @@ fn main() {
     // i.e the threads that we spawn are the producers and we are the consumer
     let (tx, rx) = mpsc::channel();
 
+    let mut handles = vec![];
     for (i, t) in tasks.into_iter().enumerate() {
-
         let tx_clone = tx.clone();
         let name = format!("Worker-{}", i);
-        let _ = thread::Builder::new().name(name).spawn(move || {
-            let task = t.clone();
-            let worker = Worker::create_worker(i as u32);
-            let result = worker.process_task(task);
-            tx_clone.send(result).unwrap();
-        });
 
+        let handle = thread::Builder::new()
+            .name(name)
+            .spawn(move || {
+                let task = t.clone();
+                let worker = Worker::create_worker(i as u32);
+                let result = worker.process_task(task);
+                tx_clone.send(result).unwrap();
+            })
+            .unwrap();
+
+        handles.push(handle);
     }
 
-    // loop through all results
-    for received in &rx {
-        println!("The main thread received {}", received);
+    // Without checking for an explicit count
+    // we'll be stuck in this loop forever, waiting for more receives.
+    // Why does it think there are more receives? Because each thread
+    // receives a tx clone, which means there is/are still Senders
+    // and those are only going to be dropped at end of scope.
+    // Although why that scope continues to linger, I do not know.
+    let mut received_count = 0;
+    while received_count < num_tasks {
+        if let Ok(received) = rx.recv() {
+            println!("{}", received);
+            received_count += 1;
+        } else {
+            // The channel has been closed and all messages have been received
+            break;
+        }
     }
 
-    // Drop the receiver to close off things
-    drop(rx);
+    for handle in handles {
+        handle.join().unwrap();
+    }
 }
